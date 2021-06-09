c6e69b50bac67bb0fa224e7ef767b6d7b332a924
brunoplasdan <bruno.sousa@plasdan.pt>
2021-06-09 09:24:53 +0100

    SignUp User

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 
 
 const postRoute = require('./routes/posts');
+const userRoute = require('./routes/user');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/posts", postRoute);
+app.use("/user", userRoute);
 
 module.exports = app;
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,28 @@
 const { User } = require('../models');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+function signUp(req,res) {
+    
+    const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    User.create(user).then(response => {
+
+        res.status(201).json({
+            message: "User created successfully"
+        });
+
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong"
+        });
+    });
+}
+
+module.exports = {
+    signUp: signUp
+}
